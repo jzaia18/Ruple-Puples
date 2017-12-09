1a2873a5e2565850c6ddb8bffe219e7771f8d90e
Jake Zaia <jzaia@stuy.edu>
2017-12-09 15:08:26 -0500

    Add main control file

--- control.c
+++ control.c
@@ -12,28 +12,62 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define DEBUGGING 1 // Set to or as appropriate
+#define DEBUGGING 0 // Set to or as appropriate
 #define SEMKEY 420
 #define SHMKEY 421
 
+
+union semun { //fancy!
+  int              val;    /* Value for SETVAL */
+  struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+  unsigned short  *array;  /* Array for GETALL, SETALL */
+  struct seminfo  *__buf;  /* Buffer for IPC_INFO (Linux-specific) */
+};
+
+
+
+// Removes story, shm, and sem
 void create_story() {
-  semget(SEMKEY, 1, IPC_CREAT | IPC_EXCL | 0644);
-  shmget(SHMKEY, 1, IPC_CREAT | IPC_EXCL | 0644);
-  open("story.txt", O_RDWR | O_CREAT | O_TRUNC);
+  union semun su;
+  su.val = 1;
+  int sd = semget(SEMKEY, 1, IPC_CREAT | IPC_EXCL | 0600);
+  semctl(sd, 0, SETVAL, su);
+  shmget(SHMKEY, sizeof(int), IPC_CREAT | IPC_EXCL | 0600);
+  open("story.txt", O_RDWR | O_CREAT | O_TRUNC, 0744);
   printf("Created story\n");
 }
 
 
+// View the story (and shm & sem)
 void view_story() {
-  //TODO
+  if (DEBUGGING) {
+    printf("DEBUG INFO:\n");
+    int sd = semget(SEMKEY, 1, 0);
+    printf("Semaphore value: %d\n", semctl(sd, 0, GETVAL));
+
+    sd = shmget(SHMKEY, sizeof(int), 0);
+    int *i = (int *)shmat(sd, 0, SHM_RDONLY);
+    printf("Shared mem Value: %d\n\n", *i);
+    shmdt(i);
+  }
+
+  printf("The story:\n");
+
+  if (!fork())
+    execlp("cat", "cat", "story.txt", NULL);
 }
 
+
+// Removes story, shm, and sem
 void remove_story() {
-  int sd = semget(SEMKEY, 1, 0);
+  view_story(); // Prints out story
+
+  int sd = 0; //Later to be used as sem/shm desc
+  wait(&sd);
+  sd = semget(SEMKEY, 1, 0);
   semctl(sd, 0, IPC_RMID);
-
-  //TODO: shm
-
+  sd = shmget(SHMKEY, 1, 0);
+  shmctl(sd, 0, IPC_RMID);
   remove("story.txt");
 
 }
