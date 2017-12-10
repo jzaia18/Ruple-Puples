b96beeaa3e3567d133df037aea19b8d2f0e11067
Adris <adrisj7@gmail.com>
2017-12-10 18:30:36 -0500

    Utilize directive constants in control file and add extra print info

--- control.c
+++ control.c
@@ -1,39 +1,16 @@
-#include <errno.h>
-#include <stdio.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/ipc.h>
-#include <sys/sem.h>
-#include <sys/shm.h>
-#include <unistd.h>
-#include <signal.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define DEBUGGING 0 // Set to or as appropriate
-#define SEMKEY 420
-#define SHMKEY 421
-
-
-union semun { //fancy!
-  int              val;    /* Value for SETVAL */
-  struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
-  unsigned short  *array;  /* Array for GETALL, SETALL */
-  struct seminfo  *__buf;  /* Buffer for IPC_INFO (Linux-specific) */
-};
-
+#include "shared_header.h"
 
 
 // Removes story, shm, and sem
 void create_story() {
   union semun su;
   su.val = 1;
-  int sd = semget(SEMKEY, 1, IPC_CREAT | IPC_EXCL | 0600);
+  int sd = semget(SEMKEY, 1, IPC_CREAT | IPC_EXCL | 0644);
   semctl(sd, 0, SETVAL, su);
-  shmget(SHMKEY, sizeof(int), IPC_CREAT | IPC_EXCL | 0600);
-  open("story.txt", O_RDWR | O_CREAT | O_TRUNC, 0744);
+
+  shmget(SHMKEY, sizeof(int), IPC_CREAT | IPC_EXCL | 0644);  
+
+  open(STORY_PATH, O_RDWR | O_CREAT | O_TRUNC, 0744);
   printf("Created story\n");
 }
 
@@ -54,13 +31,13 @@ void view_story() {
   printf("The story:\n");
 
   if (!fork())
-    execlp("cat", "cat", "story.txt", NULL);
+    execlp("cat", "cat", STORY_PATH, NULL);
 }
 
 
 // Removes story, shm, and sem
 void remove_story() {
-  view_story(); // Prints out story
+  //view_story(); // Prints out story
 
   int sd = 0; //Later to be used as sem/shm desc
   wait(&sd);
@@ -68,8 +45,8 @@ void remove_story() {
   semctl(sd, 0, IPC_RMID);
   sd = shmget(SHMKEY, 1, 0);
   shmctl(sd, 0, IPC_RMID);
-  remove("story.txt");
-
+  remove(STORY_PATH);
+  printf("Story removed\n");
 }
 
 int main(int argc, char** argv) {
